2d5042b953669be6e21d57c356b5544bac2af2ed
shtnkgm <nakagami.shota@gmail.com>
2018-09-19 19:22:36 +0900

    Initial Commit

---
+++ ImageTransition/ImageTransition.h
@@ -0,0 +1,19 @@
+//
+//  ImageTransition.h
+//  ImageTransition
+//
+//  Created by Shota Nakagami on 2018/09/19.
+//  Copyright © 2018年 Shota Nakagami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImageTransition.
+FOUNDATION_EXPORT double ImageTransitionVersionNumber;
+
+//! Project version string for ImageTransition.
+FOUNDATION_EXPORT const unsigned char ImageTransitionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageTransition/PublicHeader.h>
+
+
---
+++ ImageTransitionTests/ImageTransitionTests.swift
@@ -0,0 +1,34 @@
+//
+//  ImageTransitionTests.swift
+//  ImageTransitionTests
+//
+//  Created by Shota Nakagami on 2018/09/19.
+//  Copyright © 2018年 Shota Nakagami. All rights reserved.
+//
+
+import XCTest
+@testable import ImageTransition
+
+class ImageTransitionTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
