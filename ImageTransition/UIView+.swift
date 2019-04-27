d7a6b9df0a88561c42302f00fc784ffcc93fa5a2
shtnkgm <shtnkgm@gmail.com>
2019-04-28 01:16:18 +0900

    Fix warning

--- ImageTransition/UIView+.swift
+++ ImageTransition/UIView+.swift
@@ -9,16 +9,16 @@
 import UIKit
 
 internal extension UIView {
-    internal func convertFrame(to view: UIView) -> CGRect {
+    func convertFrame(to view: UIView) -> CGRect {
         return convert(bounds, to: view)
     }
 
-    internal func convertCenter(to view: UIView) -> CGPoint {
+    func convertCenter(to view: UIView) -> CGPoint {
         let frame = convertFrame(to: view)
         return CGPoint(x: frame.minX + frame.width / 2.0, y: frame.minY + frame.height / 2.0)
     }
 
-    internal func addSubviews(_ views: UIView...) {
+    func addSubviews(_ views: UIView...) {
         views.forEach {
             addSubview($0)
         }
